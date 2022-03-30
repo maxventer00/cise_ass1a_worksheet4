6f3c1da61ba2c517475dd040ed2a7697bb94832e
max.venter00 <max.venter00@gmail.com>
2022-03-31 12:43:02 +1300

    Setup homejs

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>How to use SEPER</h2>
+      <p>I am not sure what to put on the homepage</p>
+      <p>A nice logo? Instructions?</p>
+    </div>
+  );
+}
+
+export default Home;
